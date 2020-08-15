215d434567f6a50857786347ce0d16f841c9c1e1
NotTacoz <tacozpvp@gmail.com>
2020-08-15 14:41:54 +0800

    removed 1 print

--- cogs/fun.py
+++ cogs/fun.py
@@ -44,8 +44,6 @@ class Fun(commands.Cog):
         aaaaa = random.randint(1, 101)
         print("{} issued .dankrate 💸".format(message_author))
 
-        print(message)
-
         if message == "megalovania" or message == "tacoz" or message == "TacoBot" or message == "<@!566193825874182164>" or message == "<@!***REMOVED***>":
             embedVar = discord.Embed(
                 title="Dank r8 Machine",
