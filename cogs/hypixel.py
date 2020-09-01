83e0366db171f715f65bacb45ab549d4f96ec8f2
NotTacoz <tacozpvp@gmail.com>
2020-09-01 21:44:31 +0800

    overlay

--- cogs/hypixel.py
+++ cogs/hypixel.py
@@ -157,7 +157,7 @@ class Hypixel(commands.Cog):
                                inline=True)
 
             embedVar.set_thumbnail(
-                url=f"https://crafatar.com/avatars/{uuid}"
+                url=f"https://crafatar.com/avatars/{uuid}&overlay"
             )  #alternatives: https://crafatar.com/avatars/uuid https://crafatar.com/renders/head/uuid https://crafatar.com/renders/body/uuid
             embedVar.set_footer(text=footer)
             await ctx.send(embed=embedVar)
@@ -323,7 +323,7 @@ class Hypixel(commands.Cog):
                                inline=True)
 
             embedVar.set_thumbnail(
-                url=f"https://crafatar.com/avatars/{uuid}"
+                url=f"https://crafatar.com/avatars/{uuid}&overlay"
             )  #alternatives: https://crafatar.com/avatars/uuid https://crafatar.com/renders/head/uuid https://crafatar.com/renders/body/uuid
             embedVar.set_footer(text=footer)
             await ctx.send(embed=embedVar)
