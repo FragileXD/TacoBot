0aaabaa8b67c5eb8fbac4f25798637762294b72e
NotTacoz <tacozpvp@gmail.com>
2020-09-02 16:14:19 +0800

    footer update

--- cogs/hypixel.py
+++ cogs/hypixel.py
@@ -11,7 +11,7 @@ from discord.ext import commands
 from discord.ext.commands import has_permissions, CheckFailure, Bot
 from datetime import timedelta
 
-footer = "Made with ❤️ by Tacoz!"
+footer = "『 TacoBot ✦ Tacoz 』"
 start_time = time.monotonic()
 apikey = "a54ce218-4fd5-4798-9b4b-6c74efac3456"
 
