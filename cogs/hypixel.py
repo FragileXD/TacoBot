c8113be6b45176fae590731b8b81b7d66dcd18f3
NotTacoz <tacozpvp@gmail.com>
2020-09-13 10:54:53 +0800

    2

--- cogs/hypixel.py
+++ cogs/hypixel.py
@@ -790,6 +790,113 @@ class Hypixel(commands.Cog):
                     name="Games Played", value=f"``{gamesplayed}``", inline=True
                 )
 
+            solo = discord.Embed(
+                title=f"{full}",
+                color=15105570,
+                url=f"https://hypixel.net/player/{msg}",
+            )
+            solo.set_author(
+                name="Overall Bedwars Stats",
+                icon_url="https://statsify.net/img/assets/hypixel/bedwars.png",
+            )
+            solo.add_field(name="Stars", value=f"``{bwlevel}☆``", inline=True)
+            try:
+                solo.add_field(name="Coins", value=f"``{bwcoins:,}``", inline=True)
+            except:
+                solo.add_field(name="Coins", value=f"``{bwcoins}``", inline=True)
+            try:
+                solo.add_field(
+                    name="Winstreak", value=f"``{bwwinstreak:,}``", inline=True
+                )
+            except:
+                solo.add_field(
+                    name="Winstreak", value=f"``{bwwinstreak}``", inline=True
+                )
+            solo.add_field(name="Wins", value=f"``{bwwins}``", inline=True)
+            try:
+                solo.add_field(name="Losses", value=f"``{bwlosses:,}``", inline=True)
+            except:
+                solo.add_field(name="Losses", value=f"``{bwlosses}``", inline=True)
+            try:
+                solo.add_field(
+                    name="Win Loss Ratio", value=f"``{bwwinlossratio:,}``", inline=True
+                )
+            except:
+                solo.add_field(
+                    name="Win Loss Ratio", value=f"``{bwwinlossratio}``", inline=True
+                )
+            try:
+                solo.add_field(name="Kills", value=f"``{bwkills:,}``", inline=True)
+            except:
+                solo.add_field(name="Kills", value=f"``{bwkills}``", inline=True)
+            try:
+                solo.add_field(name="Deaths", value=f"``{bwdeaths:,}``", inline=True)
+            except:
+                solo.add_field(name="Deaths", value=f"``{bwdeaths}``", inline=True)
+            solo.add_field(name="KDR", value=f"``{bwkdr}``", inline=True)
+            try:
+                solo.add_field(
+                    name="Final Kills", value=f"``{bwfinalkills:,}``", inline=True
+                )
+            except:
+                solo.add_field(
+                    name="Final Kills", value=f"``{bwfinalkills}``", inline=True
+                )
+            try:
+                solo.add_field(
+                    name="Final Deaths", value=f"``{bwfinaldeaths:,}``", inline=True
+                )
+            except:
+                solo.add_field(
+                    name="Final Deaths", value=f"``{bwfinaldeaths}``", inline=True
+                )
+            try:
+                solo.add_field(name="Final KDR", value=f"``{bwfkdr:,}``", inline=True)
+            except:
+                solo.add_field(name="Final KDR", value=f"``{bwfkdr}``", inline=True)
+            try:
+                solo.add_field(
+                    name="Beds Lost", value=f"``{bwbedlost:,}``", inline=True
+                )
+            except:
+                solo.add_field(name="Beds Lost", value=f"``{bwbedlost}``", inline=True)
+            try:
+                solo.add_field(
+                    name="Beds Broken", value=f"``{bwbedbreak:,}``", inline=True
+                )
+            except:
+                solo.add_field(
+                    name="Beds Broken", value=f"``{bwbedbreak}``", inline=True
+                )
+            try:
+                solo.add_field(name="BBLR", value=f"``{bblr:,}``", inline=True)
+            except:
+                solo.add_field(name="BBLR", value=f"``{bblr}``", inline=True)
+            try:
+                solo.add_field(
+                    name="Finals/Game", value=f"``{finalspergame:,}``", inline=True
+                )
+            except:
+                solo.add_field(
+                    name="Finals/Game", value=f"``{finalspergame}``", inline=True
+                )
+            try:
+                solo.add_field(
+                    name="Beds/Game", value=f"``{bedspergame:,}``", inline=True
+                )
+            except:
+                solo.add_field(
+                    name="Beds/Game", value=f"``{bedspergame}``", inline=True
+                )
+            try:
+                solo.add_field(
+                    name="Games Played", value=f"``{gamesplayed:,}``", inline=True
+                )
+            except:
+                solo.add_field(
+                    name="Games Played", value=f"``{gamesplayed}``", inline=True
+                )
+
             solo.set_thumbnail(
                 url=f"https://crafatar.com/avatars/{uuid}?default=MHF_Steve&overlay"
             )  # alternatives: https://crafatar.com/avatars/uuid https://crafatar.com/renders/head/uuid https://crafatar.com/renders/body/uuid
