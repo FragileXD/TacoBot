3d4a5954077cbde4141c14df573e4c4244611d35
NotTacoz <tacozpvp@gmail.com>
2020-09-18 16:40:10 +0800

    bal command

--- test.py
+++ test.py
@@ -1,18 +1,3 @@
-from utils.data import getJSON
-import pymongo
-from pymongo import MongoClient
+import random
 
-config = getJSON("config.json")
-
-cluster = MongoClient(config.mongoclient)
-db = cluster["coins"]
-collection = db["coins"]
-print(db)
-post = {
-    "_id": "testid",
-    "bank": 0,
-    "maxbank": 100,
-    "purse": 0,
-}
-collection.insert_one(post)
-print("completed")
+print("{:06x}".format(random.randint(0, 0xFFFFFF)))
