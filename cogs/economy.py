858e82f5446869104cf63288e82e0230a2618ed1
NotTacoz <tacozpvp@gmail.com>
2020-09-18 16:17:45 +0800

    strat command

--- cogs/economy.py
+++ cogs/economy.py
@@ -15,8 +15,8 @@ from utils.data import getJSON
 config = getJSON("config.json")
 
 cluster = MongoClient(config.mongoclient)
-db = cluster["coins"]
-collection = db["coins"]
+# db = cluster["coins"]
+# collection = db["coins"]
 
 footer = "『 TacoBot ✦ Tacoz 』"
 start_time = time.monotonic()
@@ -30,7 +30,25 @@ class Economy(commands.Cog):
         name="start", description="Start your economical adventure!", aliases=["create"]
     )
     async def start(self, ctx):
-        pass
+        try:
+            db = cluster["coins"]
+            collection = db["coins"]
+            post = {
+                "_id": ctx.author.id,
+                "bank": 0,
+                "maxbank": 100,
+                "purse": 0,
+            }
+            collection.insert_one(post)
+            embed1 = discord.Embed(title=":white_check_mark: Success!", description=f"{ctx.author.mention} your have been registered!",)
+            await ctx.send(embed=embed1)
+    except pymongo.errors.DuplicateKeyError:
+            embed1 = discord.Embed(
+                title="Error!",
+                description=f"Sorry {ctx.author.mention} your already registered!",
+            )
+            await ctx.send(embed=embed1)
+            return
 
 
 def setup(bot):
