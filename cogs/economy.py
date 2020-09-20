ec6fafabb2717c66a6aed44502703e764b67e338
NotTacoz <tacozpvp@gmail.com>
2020-09-20 09:05:26 +0800

    economy update

--- cogs/economy.py
+++ cogs/economy.py
@@ -22,37 +22,35 @@ footer = "『 TacoBot ✦ Tacoz 』"
 start_time = time.monotonic()
 
 
+def balancecheck(userid):
+    db = cluster["coins"]
+    collection = db["coins"]
+    try:
+        post = {
+            "_id": userid,
+            "bank": 0,
+            "maxbank": 100,
+            "purse": 0,
+        }
+        collection.insert_one(post)
+    except pymongo.errors.DuplicateKeyError:
+        query = {"_id": userid}
+        user = collection.find(query)
+        for result in user:
+            bank = result["bank"]
+            maxbank = result["maxbank"]
+            purse = result["purse"]
+            withdraw = maxbank - bank
+            if bank > maxbank:
+                collection.update_one({"_id": userid}, {"$set": {"bank": bank}})
+                collection.update_one(
+                    {"_id": userid}, {"$set": {"purse": purse + withdraw}}
+                )
+
+
 class Economy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    def balancecheck(self, userid):
-        db = cluster["coins"]
-        collection = db["coins"]
-        query = {"_id": ctx.author.id}
-        user = collection.find(query)
-        try:
-            post = {
-                "_id": userid,
-                "bank": 0,
-                "maxbank": 100,
-                "purse": 0,
-            }
-            collection.insert_one(post)
-        except pymongo.errors.DuplicateKeyError:
-            for result in user:
-                bank = result["bank"]
-                maxbank = result["maxbank"]
-                purse = result["purse"]
-                withdraw = maxbank - bank
-                if bank > maxbank:
-                    collection.update_one(
-                        {"_id": ctx.author.id}, {"$set": {"bank": userbal}}
-                    )
-                    collection.update_one(
-                        {"_id": ctx.author.id}, {"$set": {"purse": purse + withdraw}}
-                    )
-                return await ctx.send(f"Withdrawn ${withdraw}")
 
     @commands.command(
         name="start", description="Start your economical adventure!", aliases=["create"]
@@ -102,14 +100,14 @@ class Economy(commands.Cog):
             maxbank = result["maxbank"]
             purse = result["purse"]
 
-            balancecheck(self, ctx.author.id)
+            balancecheck(ctx.author.id)
 
             embed1 = discord.Embed(
                 title=f"{ctx.author}'s balance",
                 color=color,
             )
             embed1.add_field(name="Purse:", value=purse, inline=True)
-            embed1.add_field(name="Bank:", value=f"{userbal}/{maxbank}", inline=True)
+            embed1.add_field(name="Bank:", value=f"{bank}/{maxbank}", inline=True)
             await ctx.send(embed=embed1)
 
     @commands.command(
@@ -117,28 +115,78 @@ class Economy(commands.Cog):
         description="Deposit money into your bank",
         aliases=["dep"],
     )
-    @commands.cooldown(1, 20, commands.BucketType.user)
-    async def deposit(self, ctx):
-        color = int("{:06x}".format(random.randint(0, 0xFFFFFF)), 16)
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def deposit(self, ctx, amount: str):
+        # color = int("{:06x}".format(random.randint(0, 0xFFFFFF)), 16)
         db = cluster["coins"]
         collection = db["coins"]
         query = {"_id": ctx.author.id}
         user = collection.find(query)
+
+        balancecheck(ctx.author.id)
 
         for result in user:
             bank = result["bank"]
             maxbank = result["maxbank"]
             purse = result["purse"]
             if bank > maxbank:
-                collection.update_one(
-                    {"_id": ctx.author.id}, {"$set": {"bank": userbal}}
-                )
+                collection.update_one({"_id": ctx.author.id}, {"$set": {"bank": bank}})
 
-            if bank < 0:
+            if bank < 0 or purse < 0:
                 await ctx.send("you are in debt, how is this even possible???")
+                if purse < 0 and bank > 0 and 0 - bank > purse:
+                    await ctx.send(
+                        "since your purse is negative, i will put it back to 0 by taking away funds from your bank. this is what you get for cheating the system lmao"
+                    )
+                    withdraw = -purse
+                    collection.update_one(
+                        {"_id": ctx.author.id}, {"$set": {"bank": bank - withdraw}}
+                    )
+                    collection.update_one(
+                        {"_id": ctx.author.id}, {"$set": {"purse": purse + withdraw}}
+                    )
             else:
-                if maxbank >= bank:
-                    deposit = maxbank - bank
+                try:
+                    if maxbank >= bank + int(amount):
+                        deposit = int(amount)
+                        collection.update_one(
+                            {"_id": ctx.author.id}, {"$set": {"bank": bank + deposit}}
+                        )
+                        collection.update_one(
+                            {"_id": ctx.author.id}, {"$set": {"purse": purse - deposit}}
+                        )
+                    elif amount.lower() == "all":
+                        if maxbank - bank > purse:
+                            deposit = purse
+                            collection.update_one(
+                                {"_id": ctx.author.id},
+                                {"$set": {"bank": bank + deposit}},
+                            )
+                            collection.update_one(
+                                {"_id": ctx.author.id},
+                                {"$set": {"purse": purse - deposit}},
+                            )
+                        elif purse >= maxbank - bank:
+                            deposit = maxbank - bank
+                            collection.update_one(
+                                {"_id": ctx.author.id},
+                                {"$set": {"bank": bank + deposit}},
+                            )
+                            collection.update_one(
+                                {"_id": ctx.author.id},
+                                {"$set": {"purse": purse - deposit}},
+                            )
+                    else:
+                        await ctx.send("wtf are you doing??? that is an invalid input")
+                except ValueError:
+                    await ctx.send("input a number or just say ``all`` dummy")
+
+    @deposit.error
+    async def deposit_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Please Input something after the command")
+        else:
+            raise (error)
 
 
 def setup(bot):
