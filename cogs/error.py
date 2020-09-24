5b04fcafcc0156ec965b3365b214226d155447bd
NotTacoz <tacozpvp@gmail.com>
2020-09-24 17:40:46 +0800

    mock update

--- cogs/error.py
+++ cogs/error.py
@@ -43,9 +43,6 @@ class Errors(commands.Cog):
             return
 
         if isinstance(e, commands.CommandOnCooldown):
-            if await self.bot.get_cog("Database").is_premium(ctx.author.id):
-                e.retry_after -= (2 / 5) * e.cooldown.per
-
             if round(e.retry_after, 2) <= 0:
                 await ctx.reinvoke()
 
